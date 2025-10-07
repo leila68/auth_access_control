861b21a4a90f9c2ee7e69e326bb32ecb0415c390
Leila <leila.cheshmi@gmail.com>
2025-10-07 15:43:49 -0400

    add user and setting pages for admin dashboard

---
+++ src/pages/settings.tsx
@@ -0,0 +1,5 @@
+const Settings = () => {
+    return <div className="text-gray-500">Setting management coming soon...</div>;
+  };
+  export default Settings;
+  
---
+++ src/pages/users.tsx
@@ -0,0 +1,5 @@
+const Users = () => {
+    return <div className="text-gray-500">Users management coming soon...</div>;
+  };
+  export default Users;
+  
